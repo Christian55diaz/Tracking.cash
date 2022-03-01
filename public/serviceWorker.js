f87711671873824c85825010f2e1cb1c0988a061
Christian Diaz <christian55diaz@gmail.com>
2022-03-01 04:12:26 -0500

    installing service worker

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,3 +1,5 @@
+const { response } = require("express");
+
 // we need to acquire cahce
 const CACHE_NAME = 'my-site-cache-v1';
 const DATA_CACHE_NAME = 'data-cache-v1';
@@ -18,3 +20,17 @@ const FILES_TO_CACHE = [
     "/icons/icon-384x384.png",
     "/icons/icon-512x512.png"
 ];
+
+//we also need to install the service worker so it actually works
+self.addEventListener('install', function (e) {
+e.waitUntil(
+    cahces.open(CACHE_NAME).then(cahce => {
+        //console logging so that we know the test ran correct if we got this message
+        console.log('Files have been CACHED!');
+        //return all the files cache to "FILES_TO_CACHE"
+        return Cache.addAll(FILES_TO_CACHE);
+    })
+);
+self.skipWaiting();
+});
+
